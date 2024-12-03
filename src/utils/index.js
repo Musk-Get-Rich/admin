35626f45904fccccd5da155f7a90592e608d4273
maske <maske666@gmail.com>
2024-12-03 17:59:35 +0800

    d

--- src/utils/index.js
+++ src/utils/index.js
@@ -171,7 +171,6 @@ export const formatTime = (date, fmt = 'YYYY-MM-DD hh:mm:ss') => {
 }
 
 export const imageSrc = (image) => {
-  console.log(`/src/assets/images/${image}`);
   return new URL(`/src/assets/images/${image}`, import.meta.url).href;
 }
 
