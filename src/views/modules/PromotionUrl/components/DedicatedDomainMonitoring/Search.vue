4711f3bf0ae9fc0e12a72ac2d3eca81fd5f5bfd7
leo <arvin@arvindeMacBook-Pro.local>
2024-12-07 19:47:25 +0300

    更新推广网址静态页面

---
+++ src/views/modules/PromotionUrl/components/DedicatedDomainMonitoring/Search.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="flex flex-col">
+    <div class="mb-30 w-300">
+      <el-input v-model="form.username" placeholder="请输入关键字" />
+    </div>
+    <TimeSelect
+      @search="onSearch"
+      @clear="onRefresh"
+    />
+  </div>
+</template>
+
+<script setup>
+import TimeSelect from "@/components/TimeSelect"
+
+const emit = defineEmits(['search', 'refresh'])
+
+const form = ref({
+  username: ''
+})
+
+// 搜索
+const onSearch = (val) => {
+  // getTableData(val)
+  emit('search', val)
+}
+
+const onRefresh = () => {
+  emit('refresh')
+  // getTableData({})
+}
+</script>
+
+<style lang="scss" scoped>
+:deep(.el-form-item--default) {
+  margin-bottom: 0;
+}
+</style>
